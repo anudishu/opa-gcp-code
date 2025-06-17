7901ce4a63bf2b5e51e5baa8cf4703b35e5bf457
anudishu <lyfedge799@gmail.com>
2025-06-17 15:46:05 +0530

    demo

--- provider.tf
+++ provider.tf
@@ -1,11 +1,7 @@
 
 
-variable "google_credentials" {
-  type = string
-}
-
 provider "google" {
-  credentials = var.google_credentials
+ 
   project     = "projecta-418002"
   region      = "us-central1"
 }
