35a61f279b7cb91720234b79baa6c45990d91db3
anudishu <lyfedge799@gmail.com>
2025-06-25 13:39:53 +0530

    added backend GCS

--- provider.tf
+++ provider.tf
@@ -6,3 +6,10 @@ provider "google" {
   region      = "us-central1"
   credentials = file(var.google_credentials_file)
 }
+
+terraform {
+  backend "gcs" {
+    bucket  = "sumitk-bucket"   # <-- replace with your bucket name
+    prefix  = "opa/terraform.tfstate"      # folder path inside bucket
+  }
+}
